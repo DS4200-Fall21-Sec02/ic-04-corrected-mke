525e4e33c07c603670a7c0e0c488d1b2f0753453
EstherAnem <86205146+EstherAnem@users.noreply.github.com>
2021-10-05 13:20:40 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,3 +20,50 @@ let svg1 = d3.select('#d3-container')
   .style('border', 'solid')
   .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
 
+
+
+d3.csv("data/data.csv").then( function(data) {
+
+const groups = data.map(d => d.X)
+
+var xScale = d3.scaleBand()
+      .domain(groups)
+      .range([0, width])
+      .padding([0.5])
+
+const yScale = d3.scaleLinear()
+    .domain([0, 50])
+    .range([ height, 0 ]);
+
+var g = svg1.append("g")
+               .attr("transform",`translate(${margin.left},${margin.top})`);
+
+       
+xScale.domain(data.map(function(d) { return d.X; }));
+yScale.domain([0, d3.max(data, function(d) { return d.Y; })]);
+
+g.append("g")
+         .attr("transform", `translate(0, ${height})`)
+         .call(d3.axisBottom(xScale).tickSize(0));
+
+g.append("g")
+         .call(d3.axisLeft(yScale).tickFormat(function(d){
+             return d;
+         }).ticks(10))
+         .append("text")
+         .attr("y", 6)
+         .attr("dy", "0.71em")
+         .attr("text-anchor", "end")
+         .text("value");
+
+g.selectAll(".bar")
+         .data(data)
+         .enter().append("rect")
+         .attr("class", "bar")
+         .attr("x", function(d) { return xScale(d.X); })
+         .attr("y", function(d) { return yScale(d.Y); })
+         .attr("width", xScale.bandwidth())
+         .attr("height", function(d) { return height - yScale(d.Y); });
+
+
+});
